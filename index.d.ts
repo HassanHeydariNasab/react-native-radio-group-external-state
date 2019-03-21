32214982cb1ff780d2a6fc8ebb5d44839bf1f6f4
hsn6 <hsn6@tuta.io>
2019-03-21 22:16:01 +0330

    Fix inheritance of ViewProps

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module 'react-native-radio-group-external-state' {
     import { ViewProps } from 'react-native'
     import { ReactElement } from 'react'
-    interface RadioGroupProps {
+    interface RadioGroupProps extends ViewProps {
         options: object[]
         value: string
         onChange?: () => void
@@ -12,6 +12,6 @@ declare module 'react-native-radio-group-external-state' {
         rtl?: boolean
     }
     export default function RadioGroup(
-        props: RadioGroupProps<ViewProps>
+        props: RadioGroupProps
     ): ReactElement | null
 }
